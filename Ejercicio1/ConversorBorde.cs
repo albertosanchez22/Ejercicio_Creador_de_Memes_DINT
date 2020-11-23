f04dac3d5a56828f7639c656c94c8e9f5a600095
Alberto Sánchez <alberto.sanchez@iesdoctorbalmis.com>
2020-11-23 14:16:25 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio1/ConversorBorde.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ejercicio1
+{
+    class ConversorBorde : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Boolean)value == true)
+            {
+                return 2;
+            } else
+                return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
